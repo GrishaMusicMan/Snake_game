a40c1ff1cd979f1051cba13fb937019825ed3b9c
Grisha <parfen20@rambler.ru>
2022-08-20 12:42:18 +0200

    start progect

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+FRAME_COLOR = [0, 255, 204]
+
+size = [400, 600]
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Змейка")
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print('Выход')
+            pygame.quit()
+
+        screen.fill(FRAME_COLOR)
+        pygame.display.flip()
